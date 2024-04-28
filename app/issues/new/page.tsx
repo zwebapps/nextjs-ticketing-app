d649079d2be1f3d4145509cc979f5adc022a10f6
Zahoor Ahmed <zdev1989@gmail.com>
2024-04-28 02:24:56 +0200

    Added simple editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import React from "react";
 
 const CreateNewIssue = () => {
@@ -7,7 +9,7 @@ const CreateNewIssue = () => {
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title">
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE/>
       <Button>Submit New Issue</Button>
     </div>
   );
